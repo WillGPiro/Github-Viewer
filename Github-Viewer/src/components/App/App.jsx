c3336ef6b861219ce7e27df435e89a9ba65d2caa
Will G Piro <willgpiro@gmail.com>
2020-04-28 19:09:30 -0700

    add fetch call to github

--- Github-Viewer/src/components/App/App.jsx
+++ Github-Viewer/src/components/App/App.jsx
@@ -3,22 +3,10 @@ import GithubViewer from '../../containers/GithubViewer.jsx';
 import Repos from '../Repos/Repos.jsx';
 
 export default function App() {
-  const repos = [
-    {
-      id: 1,
-      name: 'About-Me-Guessing-Game',
-      url: 'https://api.github.com/repos/Shaka2Pass/About-Me-Guessing-Game'
-    },
-    {
-      id: 2,
-      name: 'auth-lab',
-      url: 'https://api.github.com/repos/Shaka2Pass/auth-lab'
-    }
-  ];
+ 
   return (
     <>
       <GithubViewer />,
-      <Repos repos={repos}/>
     </>
   );
 
--- Github-Viewer/src/containers/GithubViewer.jsx
+++ Github-Viewer/src/containers/GithubViewer.jsx
@@ -4,6 +4,7 @@ import UsernameInput from '../components/Users/UsernameInput.jsx';
 import User from '../components/Users/User.jsx';
 import Repos from '../components/Repos/Repos.jsx';
 import Repo from '../components/Repos/Repo.jsx';
+import { fetchRepos } from '../services/github.js';
 
 export default class GithubViewer extends Component {
   state = {
@@ -11,7 +12,13 @@ export default class GithubViewer extends Component {
     name: 'monalisa octocat',
     followers: '20',
     following: '20',
-    url: 'https://api.github.com/users/octocat'
+    url: 'https://api.github.com/users/octocat',
+    repos:[]
+  }
+
+  componentDidMount() {
+    fetchRepos()
+      .then(repos => this.setState({ repos }));
   }
 
  onNameChange = ({ target }) => {
@@ -20,13 +27,14 @@ export default class GithubViewer extends Component {
  }
 
  render() {
-   const { username, name, followers, following, url } = this.state;
+   const { username, name, followers, following, url, repos } = this.state;
    return (
      <>
        <UsernameInput username={username} onNameChange={this.onNameChange} />
        <Username username={username}/>,
        <User name={name} followers={followers} following={following} url={url} />
        <Repo/>
+       <Repos repos={repos} />
      </>
    );
  }
